42ac867e80580baa10998aea3944c7a8dbee31c3
Areum <tjfsu120@gmail.com>
2021-01-22 00:48:45 +0900

    기능 정의 부분 논의 필요

--- src/restaurant/refrigerator/RefrigerratorServiceImpl.java
+++ src/restaurant/refrigerator/RefrigerratorServiceImpl.java
@@ -10,6 +10,7 @@ import restaurant.refrigerator.dao.RestaurantRefrigeratorDaoImpl;
 public class RefrigerratorServiceImpl implements RefrigerratorService {
 
 		private RestaurantRefrigeratorDaoImpl refger;
+		
 		public RefrigerratorServiceImpl() {
 			refger = new RestaurantRefrigeratorDaoImpl();
 		}
@@ -19,18 +20,32 @@ public class RefrigerratorServiceImpl implements RefrigerratorService {
 	@Override
 	public void buying(Ingredient ing) {
 		// TODO Auto-generated method stub
+//		공급처 재료, totalMoney 차감
+//		2개를 같이 써야 하는데 service 한군데서 공급처, 냉장고 2곳에서 쓸수 있도록 설계도 개선 제안
+//		refrigeratorService 1)supplier 2)refrigerator
 		
-	}
+		}
+
 
 	@Override
 	public void updateDue(LocalDate Date) {
 		// TODO Auto-generated method stub
+		
+/*		기능 정의 필요
+		유통기한 무한?인지 유한인지 정하고 무한이면 유통기한 수정이 굳이 필요x
+		아니면 단순히 유통기한을 변경하고 싶을 때 쓰는 것?
+		개별 유통기한 받을 수 있게 idx는 name으로 특정
+		12시간 단위로 유통기한 체크 
+*/		
 		
 	}
 
 	@Override
 	public void getByName(Scanner sc) {
 		// TODO Auto-generated method stub
+		
+		
+		
 		
 	}
 
