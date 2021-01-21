e30b1d231c38bab6017f92d8a12aef46052eb7e5
Areum <tjfsu120@gmail.com>
2021-01-21 14:13:01 +0900

    restaurantRefigeratorDaoImpl 작성 추가

--- src/restaurant/refrigerator/dao/RestaurantRefrigeratorDaoImpl.java
+++ src/restaurant/refrigerator/dao/RestaurantRefrigeratorDaoImpl.java
@@ -14,7 +14,8 @@ public class RestaurantRefrigeratorDaoImpl implements Refrigerator {
 	public void addIng(Ingredient ing) {
 		// TODO Auto-generated method stub
 		ingredients.add(ing);
-		
+//		식자재 입고(name, amount, price, due)
+
 	}
 
 	@Override
@@ -25,7 +26,7 @@ public class RestaurantRefrigeratorDaoImpl implements Refrigerator {
 		 * where name = name
 		 */
 		ArrayList<Ingredient> ingredients = new ArrayList<>();
-		
+//      식자재 검색 및 확인
 		for (Ingredient ingredient : this.ingredients) {//안에 찾는 걸 스캔
 			if (name.equals(ingredient.getName())) {
 				ingredients.add(ingredient);
@@ -36,10 +37,13 @@ public class RestaurantRefrigeratorDaoImpl implements Refrigerator {
 
 	@Override
 	public void updateDue(String name, LocalDate Date) {
-		// 유통기한
-		for (Ingredient ingredient : this.ingredients) {//안에 찾는 걸 스캔
+		// 유통기한 수정
+		for (Ingredient ingredient : this.ingredients) {
 			if (name.equals(ingredient.getName())) {
 				ingredient.setDue(Date);
+				
+//				1일이 지날수록 유통기한 변경은?
+//				default가 2100-01-01
 			}
 		}
 		
@@ -48,19 +52,34 @@ public class RestaurantRefrigeratorDaoImpl implements Refrigerator {
 	@Override
 	public void updateAmount(String name, int amount) {
 		// TODO Auto-generated method stub
-		
+//		식자재 수량 수정
+		for (Ingredient ingredient : this.ingredients) {
+			if (name.equals(ingredient.getName())) {
+				ingredient.setAmount(amount);//name으로 찾아서 amount 수정
+			} 
+/*			식자재 amount 1회 구매량 갯수
+			김:30. 단무지:10, 쌀:50, 햄:10, 계란:20, 면사리:20, 어묵:10, 대파:10, 쑥갓:5, 유부:10
+			떡 : 20, 치즈:5, 돼지고기:10, 밀가루:10, 빵가루:10, 김치:10
+*/			
+		}
 	}
 
 	@Override
 	public void deleteByName(String name) {
 		// TODO Auto-generated method stub
-		
+//		유통기한만료, 재료 소진시 식자재 삭제
+		for (Ingredient ingredient : this.ingredients) {
+			if (name.equals(ingredient.getName())) {
+				ingredients.remove(name);
+			}
+		}
 	}
 
 	@Override
 	public ArrayList<Ingredient> selectAllIng() {
 		// TODO Auto-generated method stub
-		return null;
+//		식자재 목록 확인
+		return ingredients;
 	}
 	
 
