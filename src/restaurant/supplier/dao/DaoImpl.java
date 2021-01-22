1cfc2e6b479fde235441be7960eaa401529a0f2c
100race <100race@naver.com>
2021-01-22 17:35:23 +0900

    Ingredients 직렬화 해결

--- src/restaurant/supplier/dao/DaoImpl.java
+++ src/restaurant/supplier/dao/DaoImpl.java
@@ -11,24 +11,38 @@ import java.util.ArrayList;
 
 import restaurant.food.vo.Ingredient;
 import restaurant.refrigerator.dao.Refrigerator;
-
+/**
+ * 
+ * @author kyoungju
+ *
+ */
 public class DaoImpl implements Refrigerator{
 	
 	public static final String FILE_PATH = "src/restaurant/files/ingredients.dat";
 	private static ArrayList<Ingredient> ingredients;
 	
+	/**
+	 * Dao 생성자
+	 * 파일에 객체정보를 넣어주기 위해 init()을 호출
+	 * 생성시 파일에서 리스트를 받아옴
+	 */
 	public DaoImpl() {
 		ingredients = new ArrayList<Ingredient>(); 
 		//init();
 		start();
 	}
 	
-	
+	/**
+	 * 
+	 * @return 멤버변수로 저장된 ingredients 반환
+	 */
 	public static ArrayList<Ingredient> getIngredients() {
 		return ingredients;
 	}
 
-	//실행 시 파일에서 리스트 받아옴
+	/**
+	 * 실행 시 파일에서 식자재 리스트 받아와서 ingredients에 초기화
+	 */
 	public void start() {
 		try {
 			FileInputStream fi = new FileInputStream(FILE_PATH);
@@ -49,11 +63,17 @@ public class DaoImpl implements Refrigerator{
 		}
 	}
 	
+	/**
+	 * ingredients에 ingredient 객체 하나 추가
+	 */
 	@Override
 	public void addIng(Ingredient ing) {
 		ingredients.add(ing);
 	}
 
+	/**
+	 * @return 이름으로 찾은 결과 리스트를 반환
+	 */
 	@Override
 	public ArrayList<Ingredient> searchByName(String name) {
 		ArrayList<Ingredient> sl = new ArrayList<>();
@@ -64,9 +84,12 @@ public class DaoImpl implements Refrigerator{
 		return sl;
 	}
 
+	/**
+	 * 식자재의 날짜를 바꿔주는 메서드
+	 * 이름이 같은 객체는 전부 바꾼다
+	 */
 	@Override
 	public void updateDue(String name, LocalDate date) {
-		//이름이 같은건 전부 바꾸기
 		for(int i = 0; i < ingredients.size(); i++) {
 			if(ingredients.get(i).getName().equals(name)){
 				ingredients.get(i).setDue(date);
@@ -74,10 +97,12 @@ public class DaoImpl implements Refrigerator{
 		}
 	}
 
-	
+	/**
+	 * 식자재의 재고량을 바꿔주는 메서드
+	 * 이름이 같은 객체는 전부 바꾼다
+	 */
 	@Override
 	public void updateAmount(String name, int newAmount) {
-		//이름이 같은건 전부 바꾸기
 		int oriAmount = 0;
 		for(int i = 0; i < ingredients.size(); i++) {
 			if(ingredients.get(i).getName().equals(name)){
@@ -87,6 +112,9 @@ public class DaoImpl implements Refrigerator{
 		}
 	}
 
+	/**
+	 * 식자재의 이름을 받아 삭제하는 메서드
+	 */
 	@Override
 	public void deleteByName(String name) {
 		for(int i = 0 ; i< ingredients.size(); i++) {
@@ -95,50 +123,53 @@ public class DaoImpl implements Refrigerator{
 		}
 	}
 	
-
-
+	/**
+	 * @return 식자재 목록을 반환
+	 */
 	@Override
 	public ArrayList<Ingredient> selectAllIng() {
 		return ingredients;
 	}
 	
 	
-	//첫실행시 한번만 초기화 (이후 주석처리)- Ingredient 직렬화 적용
-//	public void init() { 
-//		
-//		ArrayList<Ingredient> il = new ArrayList<Ingredient>();
-//		il.add(new Ingredient("김", 9999999, 100, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("단무지", 9999999, 50, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("쌀", 9999999, 200, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("햄", 9999999, 500, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("계란", 9999999, 200, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("면사리", 9999999, 200, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("어묵", 9999999, 250, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("대파", 9999999, 50, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("쑥갓", 9999999, 80, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("유부", 9999999, 100, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("떡", 9999999, 300, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("치즈", 9999999, 400, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("돼지고기", 9999999, 800, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("밀가루", 9999999, 150, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("빵가루", 9999999, 150, LocalDate.of(2100, 1, 1)));
-//		il.add(new Ingredient("김치", 9999999, 70, LocalDate.of(2100, 1, 1)));
-//		
-//		try {
-//			FileOutputStream fo = new FileOutputStream(FILE_PATH);
-//			ObjectOutputStream oo = new ObjectOutputStream(fo);	
-//			oo.writeObject(il);
-//			oo.close();
-//			fo.close();
-//			}
-//		catch (IOException e) {
-//			System.out.println("DaoImpl init() Error: 초기화 파일을 불러오지 못했습니다.");
-//			e.printStackTrace();
-//		}
-//		
-//	}
+	//첫실행시 한번만 초기화 (이후 주석처리)
+	public void init() { 
+		
+		ArrayList<Ingredient> il = new ArrayList<Ingredient>();
+		il.add(new Ingredient("김", 9999999, 100, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("단무지", 9999999, 50, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("쌀", 9999999, 200, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("햄", 9999999, 500, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("계란", 9999999, 200, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("면사리", 9999999, 200, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("어묵", 9999999, 250, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("대파", 9999999, 50, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("쑥갓", 9999999, 80, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("유부", 9999999, 100, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("떡", 9999999, 300, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("치즈", 9999999, 400, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("돼지고기", 9999999, 800, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("밀가루", 9999999, 150, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("빵가루", 9999999, 150, LocalDate.of(2100, 1, 1)));
+		il.add(new Ingredient("김치", 9999999, 70, LocalDate.of(2100, 1, 1)));
+		
+		try {
+			FileOutputStream fo = new FileOutputStream(FILE_PATH);
+			ObjectOutputStream oo = new ObjectOutputStream(fo);	
+			oo.writeObject(il);
+			oo.close();
+			fo.close();
+			}
+		catch (IOException e) {
+			System.out.println("DaoImpl init() Error: 초기화 파일을 불러오지 못했습니다.");
+			e.printStackTrace();
+		}
+		
+	}
 	
-	//끝날시 실행, 파일에 데이터 저장
+	/**
+	 * 파일 종료시 파일에 식사재 리스트 저장
+	 */
 	public void stop() {
 		try {
 			FileOutputStream fo = new FileOutputStream(FILE_PATH);
