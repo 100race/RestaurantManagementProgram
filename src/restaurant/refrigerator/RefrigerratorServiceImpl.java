243772739e48e919f142ad23cfab64765562ea8f
Areum <tjfsu120@gmail.com>
2021-01-22 11:27:10 +0900

    수정

--- src/restaurant/refrigerator/RefrigerratorServiceImpl.java
+++ src/restaurant/refrigerator/RefrigerratorServiceImpl.java
@@ -1,18 +1,22 @@
 package restaurant.refrigerator;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import restaurant.food.vo.Ingredient;
 import restaurant.refrigerator.dao.RestaurantRefrigeratorDaoImpl;
-
+import restaurant.supplier.dao.DaoImpl;
 
 public class RefrigerratorServiceImpl implements RefrigerratorService {
 
-		private RestaurantRefrigeratorDaoImpl refger;
+		private RestaurantRefrigeratorDaoImpl rRDao;
+		private DaoImpl sRDao;
 		
 		public RefrigerratorServiceImpl() {
-			refger = new RestaurantRefrigeratorDaoImpl();
+			this.rRDao = new RestaurantRefrigeratorDaoImpl();
+			this.sRDao = new DaoImpl();
 		}
 		
 	
@@ -21,8 +25,8 @@ public class RefrigerratorServiceImpl implements RefrigerratorService {
 	public void buying(Ingredient ing) {
 		// TODO Auto-generated method stub
 //		공급처 재료, totalMoney 차감
-//		2개를 같이 써야 하는데 service 한군데서 공급처, 냉장고 2곳에서 쓸수 있도록 설계도 개선 제안
-//		refrigeratorService 1)supplier 2)refrigerator
+		
+		
 		
 		}
 
@@ -43,27 +47,55 @@ public class RefrigerratorServiceImpl implements RefrigerratorService {
 	@Override
 	public void getByName(Scanner sc) {
 		// TODO Auto-generated method stub
-		
-		
-		
+		System.out.println("식자재 검색할 이름 입력");
+		String name = sc.next();
+		ArrayList<Ingredient> ingr = rRDao.searchByName(name);
+		if(ingr == null) {
+			System.out.println("식자재 없음");
+		}
+		else {
+			System.out.println("식자재 목록");
+			for(Ingredient i : ingr)
+				System.out.println(i);
+		}
 		
 	}
 
 	@Override
 	public void editDue(Scanner sc) {
 		// TODO Auto-generated method stub
+		System.out.println("유통기한 변경할 식자재 이름을 입력하세요");
+		String name = sc.next();
+		System.out.println("변경할 유통기한을 입력하세요");
+		LocalDate date1 = new SimpleDateFormat("dd-MM-yyyy").parse(date);
+//		updatedue 반환??
+		
+		
 		
 	}
 
 	@Override
 	public void deleteIng(Scanner sc) {
 		// TODO Auto-generated method stub
+		System.out.println("삭제할 식자재 이름을 입력하세요");
+		String name = sc.next();
+		System.out.println("해당 식자재를 삭제하실겁니까?");
+		rRDao.deleteByName(name);
+		System.out.println(name+"이 삭제되었습니다");
+		
 		
 	}
 
 	@Override
 	public void inIng(Scanner sc) {
 		// TODO Auto-generated method stub
+		System.out.println("냉장고에 넣을 식자재를 입력하세요");
+		String name;
+		String inputname;
+		if(rRDao.searchByName(name)) {
+			
+		}
+		
 		
 	}
 
@@ -73,15 +105,26 @@ public class RefrigerratorServiceImpl implements RefrigerratorService {
 		
 	}
 
+	
+	
 	@Override
 	public void getAllIng() {
 		// TODO Auto-generated method stub
-		
-	}
+		ArrayList<Ingredient> ingr = rRDao.selectAllIng();
+		if( ingr == null) {
+			System.out.println("식자재가 없습니다.");
+		}else {
+			for(Ingredient i : ingr) 
+				System.out.println(i);
+		}
 
+	}
 	@Override
 	public void PrintAllIng() {
 		// TODO Auto-generated method stub
+		System.out.println("냉장고에 있는 모든 식자재 확인하기:확인 입력");
+		ArrayList<Ingredient> ingr = rRDao.selectAllIng();
+		System.out.println(ingr);
 		
 	}
 
